8dac3c6800918c0419de992354ef724343d41906
ginxo <emingora@redhat.com>
2020-08-05 16:21:07 +0200

    BXMSPROD-918 command-treatment-factory.test improved

--- test/lib/command-treatment-factory.test.js
+++ test/lib/command-treatment-factory.test.js
@@ -37,6 +37,26 @@ test("treatCommand maven with envs", () => {
   expect(result).toEqual("maven command");
 });
 
+test("treatCommand maven more complex 1", () => {
+  // Act
+  const result = treatCommand(
+    'mvn -e -nsu -Dfull clean install -Prun-code-coverage -Pwildfly -Dcontainer=wildfly -Dcontainer.profile=wildfly -Dintegration-tests=true -Dmaven.test.failure.ignore=true -Dwebdriver.firefox.bin=/opt/tools/firefox-60esr/firefox-bin -DjvmArgs="-Xms1g -Xmx5g"'
+  );
+
+  // Assert
+  expect(result).toEqual("maven command");
+});
+
+test("treatCommand maven more complex 2", () => {
+  // Act
+  const result = treatCommand(
+    "mvn -e -T1C clean install -DskipTests -Dgwt.compiler.skip=true -Dgwt.skipCompilation=true -Denforcer.skip=true -Dcheckstyle.skip=true -Dspotbugs.skip=true -Drevapi.skip=true"
+  );
+
+  // Assert
+  expect(result).toEqual("maven command");
+});
+
 test("treatCommand no command", () => {
   // Act
   const result = treatCommand("./shell.sh");

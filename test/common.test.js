1d587457891ab2307eaa3f456dc60c572aed6d96
Enrique Mingorance Cano <emingora@redhat.com>
2020-08-10 08:49:46 +0200

    BXMSPROD-927 it's possible to define different target group for dependencies (#14)

--- test/common.test.js
+++ test/common.test.js
@@ -2,11 +2,52 @@ const { dependenciesToObject } = require("../src/lib/common");
 
 test("dependenciesToObject without branch", () => {
   // Arrange
-  const expected = { projectA: {}, projectB: {}, projectC: {}, projectD: {} };
+  const expected = {
+    projectA: { group: "defaultGroup" },
+    projectB: { group: "defaultGroup" },
+    projectC: { group: "defaultGroup" },
+    projectD: { group: "defaultGroup" }
+  };
   // Act
   const dependencies = dependenciesToObject(
-    "projectA,projectB, projectC,projectD"
+    "projectA,projectB, projectC,projectD",
+    "defaultGroup"
   );
+  // Assert
+  expect(dependencies).toEqual(expected);
+});
+
+test("dependenciesToObject without branch and with group", () => {
+  // Arrange
+  const expected = {
+    projectA: { group: "groupx" },
+    projectB: { group: "groupy" },
+    projectC: { group: "groupz" },
+    projectD: { group: "defaultGroup" }
+  };
+  // Act
+  const dependencies = dependenciesToObject(
+    "groupx/projectA,groupy/projectB, groupz/projectC,projectD",
+    "defaultGroup"
+  );
+  // Assert
+  expect(dependencies).toEqual(expected);
+});
+
+test("dependenciesToObject single dependency without group", () => {
+  // Arrange
+  const expected = { projectA: { group: "defaultGroup" } };
+  // Act
+  const dependencies = dependenciesToObject("projectA", "defaultGroup");
+  // Assert
+  expect(dependencies).toEqual(expected);
+});
+
+test("dependenciesToObject single dependency with group", () => {
+  // Arrange
+  const expected = { projectA: { group: "groupx" } };
+  // Act
+  const dependencies = dependenciesToObject("groupx/projectA", "defaultGroup");
   // Assert
   expect(dependencies).toEqual(expected);
 });
@@ -14,14 +55,21 @@ test("dependenciesToObject without branch", () => {
 test("dependenciesToObject with branch", () => {
   // Arrange
   const expected = {
-    projectA: {},
-    projectB: { mapping: { source: "7.x", target: "master" } },
-    projectC: {},
-    projectD: {}
+    projectA: { group: "defaultGroup" },
+    projectB: {
+      group: "defaultGroup",
+      mapping: { source: "7.x", target: "master" }
+    },
+    projectC: { group: "defaultGroup" },
+    projectD: {
+      group: "defaultGroup",
+      mapping: { source: "8.0.0", target: "9.1.1" }
+    }
   };
   // Act
   const dependencies = dependenciesToObject(
-    "projectA,projectB@7.x:master, projectC,projectD"
+    "projectA,projectB@7.x:master, projectC,projectD@8.0.0:9.1.1",
+    "defaultGroup"
   );
   // Assert
   expect(dependencies).toEqual(expected);

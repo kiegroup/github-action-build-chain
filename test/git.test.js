225061f229878e81a675952769c62da78ca55c28
ginxo <emingora@redhat.com>
2021-11-08 09:32:44 +0100

    git.test implemented for getVersion function

--- test/git.test.js
+++ test/git.test.js
@@ -19,7 +19,6 @@ test("merge branch into current checkout", async () => {
   const cwd = path.join(__dirname, "ws");
   const origin = path.join(__dirname, "origin");
   spawnMock.sequence.add(spawnMock.simple(0));
-  // spawnMock.setDefault(spawnMock.simple(0));
 
   // Act
   await git.merge(cwd, `file://${origin}`, "main");
@@ -61,4 +60,93 @@ test("rename branch", async () => {
     "--move",
     "feature"
   ]);
+});
+
+describe("version", () => {
+  test("getVersion major.minor.patch", async () => {
+    // Arrange
+    spawnMock.sequence.add(spawnMock.simple(0, "git version 1.2.3"));
+    // Act
+    const result = await git.getVersion();
+
+    // Assert
+    expect(spawnMock.calls.length).toBe(1);
+    const firstCall = spawnMock.calls[0];
+    expect(firstCall.command).toBe("git");
+    expect(firstCall.args).toStrictEqual([
+      "-c",
+      "user.name=GitHub",
+      "-c",
+      "user.email=noreply@github.com",
+      "--version"
+    ]);
+    expect(result).toBe("1.2.3");
+  });
+
+  test("getVersion major.minor no patch", async () => {
+    // Arrange
+    spawnMock.sequence.add(spawnMock.simple(0, "git version 1.2"));
+    // Act
+    const result = await git.getVersion();
+
+    // Assert
+    expect(spawnMock.calls.length).toBe(1);
+    const firstCall = spawnMock.calls[0];
+    expect(firstCall.command).toBe("git");
+    expect(firstCall.args).toStrictEqual([
+      "-c",
+      "user.name=GitHub",
+      "-c",
+      "user.email=noreply@github.com",
+      "--version"
+    ]);
+    expect(result).toBe("1.2");
+  });
+
+  test("getVersion only major", async () => {
+    // Arrange
+    spawnMock.sequence.add(spawnMock.simple(0, "git version 1"));
+    // Act
+    const result = await git.getVersion();
+
+    // Assert
+    expect(spawnMock.calls.length).toBe(1);
+    const firstCall = spawnMock.calls[0];
+    expect(firstCall.command).toBe("git");
+    expect(firstCall.args).toStrictEqual([
+      "-c",
+      "user.name=GitHub",
+      "-c",
+      "user.email=noreply@github.com",
+      "--version"
+    ]);
+    expect(result).toBe(undefined);
+  });
+
+  test("git command not found", async () => {
+    // Arrange
+    spawnMock.sequence.add(
+      spawnMock.simple(1, "bash: git: command not found...")
+    );
+    // Act
+    try {
+      await git.getVersion();
+    } catch (ex) {
+      expect(ex.message).toBe(
+        "command git --version failed with code 1. Error Message: bash: git: command not found..."
+      );
+    }
+
+    // Assert
+    expect(spawnMock.calls.length).toBe(1);
+    const firstCall = spawnMock.calls[0];
+    expect(firstCall.command).toBe("git");
+    expect(firstCall.args).toStrictEqual([
+      "-c",
+      "user.name=GitHub",
+      "-c",
+      "user.email=noreply@github.com",
+      "--version"
+    ]);
+  });
 });

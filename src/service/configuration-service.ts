37977e2f2da0c5dfbe204ea8a48501fa0f6e5c63
Enrique Mingorance Cano <emingora@redhat.com>
2022-06-13 17:33:38 +0200

    Add DI library (#265)
    * project scaffolding
    
    * .gitignore
    
    * report.yml flow adapted to typescript/main
    
    * logger services implemented
    
    * eslint
    
    * jest
    
    * Add DI library

--- src/service/configuration-service.ts
+++ src/service/configuration-service.ts
@@ -1,24 +1,12 @@
 import { Configuration } from "@bc/domain/configuration";
-import { EntryPoint } from "@bc/domain/entry-point";
+import { Service } from "typedi";
 
+@Service()
 export class ConfigurationService {
 
-  private readonly _configuration: Configuration;
-  private static instance: ConfigurationService;
+  private readonly _configuration?: Configuration;
 
-  // TODO: to be implemented
-  private constructor() {
-    this._configuration = { entryPoint: EntryPoint.CLI };
-  }
-
-  public static getInstance() {
-    if (ConfigurationService.instance == null) {
-      ConfigurationService.instance = new ConfigurationService();
-    }
-    return ConfigurationService.instance;
-  }
-
-  get configuration(): Configuration {
+  get configuration(): Configuration | undefined {
     return this._configuration;
   }
 }

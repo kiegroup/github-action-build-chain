1cc90ccb66eea924acfc3853e3d41b7df73fa016
Shubh Bapna <sbapna@redhat.com>
2022-07-08 14:09:15 -0400

    refactored types for definition file

--- src/types/@kie/build-chain-configuration-reader/index.d.ts
+++ src/types/@kie/build-chain-configuration-reader/index.d.ts
@@ -8,82 +8,100 @@ declare module "@kie/build-chain-configuration-reader" {
         token: string
     }
 
-    export type ProjectMapping = {
+    export type Mapping = {
         exclude?: string[]
-        dependencies: {
-            default: { source: string, target: string }[],
+        dependencies?: {
             [key: string]: { source: string, target?: string, targetExpression?: string }[]
         },
-        depenpendant: {
-            default: { source: string, target: string }[],
+        depenpendant?: {
             [key: string]: { source: string, target?: string, targetExpression?: string }[]
         }
     }
 
-    export type ProjectDependency = {
+    export type ArchiveArtifacts = {
+        path?: string | string[],
+        "if-no-files-found"?: string,
+        dependencies?: string | { project: string }[] | string[],
+        name?: string,
+        paths?: {
+            path?: string,
+            on?: string
+        }[],
+    } 
+
+    export type Build = {
+        "build-command"?: {
+            before?: {
+                current?: string | string[],
+                upstream?: string | string[],
+                downstream?: string | string[],
+                merge?: string[]
+            },
+            current?: string | string[],
+            upstream?: string | string[],
+            downstream?: string | string[],
+            after?: {
+                current?: string | string[],
+                upstream?: string | string[],
+                downstream?: string | string[],
+                merge?: string[]
+            },
+            merge?: string[]
+        },
+        skip?: boolean,
+        "archive-artifacts": ArchiveArtifacts
+    }
+
+    export type Dependency = {
         project: string,
-        mapping?: ProjectMapping
-        dependencies?: ProjectDependency[]
+        mapping?: Mapping,
+        dependencies?: string | string[] | {project: string}[]
+    }
+
+    export type DefinitionFile = {
+        version: string,
+        pre?: string | string[],
+        post?: string | string[],
+        default?: Build,
+        build?: Build,
+        dependencies?: Dependency[]
     }
 
     /** Tried to recontruct from https://github.com/kiegroup/build-chain-configuration-reader/blob/36edb075bff8d8644f1b4cb721898aa892d8e347/docs/tree-example.json */
-    export type ProjectTreeNode = {
+    export type Node = {
         project: string,
-        dependencies?: "none" | { project: string }[],
-        warning: string,
-        parents: ProjectTree,
-        children: ProjectTree,
-        repo: {
+        dependencies?: {project: string}[]
+        children?: Node[],
+        parent?: Node[],
+        repo?: {
             group: string,
             name: string
         },
-        build: {
-            "build-command": {
-                current: string[] | string,
-                upstream?: string[] | string,
-                downstream?: string[] | string,
-                after?: {
-                    upstream?: string,
-                    downstream?: string,
-                    current?: string
-                }
-            },
-            "archive-artifacts"?: {
-                path: string[],
-                "if-no-files-found": string,
-                dependencies: "none" | { project: string }[],
-                name: string,
-                paths: {
-                    path: string,
-                    on: string
-                }[],
-                skip?: boolean
-            }
-        },
-        mapping?: ProjectMapping
-    } 
+        build?: Build,
+        mapping?: Mapping
+    }
 
-    export type ProjectTree = ProjectTreeNode[];
+    export type Tree = Node[];
 
-    export function getTree(file: string, options?: BuildChainReaderOptions): ProjectTree;
+    export function getTree(file: string, options?: BuildChainReaderOptions): Tree;
 
-    export function getTreeForProject(file: string, project: string, options?: BuildChainReaderOptions): ProjectTreeNode;
+    export function getTreeForProject(file: string, project: string, options?: BuildChainReaderOptions): Node;
 
-    export function parentChainFromNode(node: ProjectTreeNode): ProjectTreeNode;
+    export function parentChainFromNode(node: Node): Tree;
 
-    export function getOrderedListForTree(file: string, options?: BuildChainReaderOptions): ProjectTree;
+    export function getOrderedListForTree(file: string, options?: BuildChainReaderOptions): Promise<Tree>;
 
-    export function getOrderedListForProject(file: string, options?: BuildChainReaderOptions): ProjectTree;
+    export function getOrderedListForProject(file: string, options?: BuildChainReaderOptions): Tree;
     
     export function treatUrl(url: string, placeholders: UrlPlaceholders): string;
 
     export function getBaseBranch(
         projectTriggeringTheJob: string,
-        projectTriggeringTheJobMapping: ProjectMapping,
+        projectTriggeringTheJobMapping: Mapping,
         currentProject: string,
-        currentProjectMapping: ProjectMapping,
+        currentProjectMapping: Mapping,
         expectedBaseBranch: string
     ): string;
 
-    export function readDefinitionFile(file: string, options?: BuildChainReaderOptions): ProjectDependency[];
+    export function readDefinitionFile(file: string, options?: BuildChainReaderOptions): Promise<DefinitionFile>;
 }

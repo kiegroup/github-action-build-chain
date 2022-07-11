9d06a8869bfaf3d6851b18ec58089c9d79b35218
Shubh Bapna <sbapna@redhat.com>
2022-07-11 10:32:39 -0400

    added more test cases for config service and added getTreatmentOption method

--- src/service/config/configuration-service.ts
+++ src/service/config/configuration-service.ts
@@ -1,23 +1,20 @@
 import Container, { Service } from "typedi";
-import { Node } from "@bc/domain/node";
 import { NodeExecutionLevel } from "@bc/domain/node-execution-level";
-import { LoggerServiceFactory } from "@bc/service/logger/logger-service-factory";
 import { constants } from "@bc/domain/constants";
-import { LoggerService } from "@bc/service/logger/logger-service";
 import { EntryPoint } from "@bc/domain/entry-point";
 import { CLIConfiguration } from "@bc/service/config/cli-configuration";
 import { ActionConfiguration } from "@bc/service/config/action-configuration";
 import { logAndThrow } from "@bc/utils/log";
 import { BaseConfiguration } from "@bc/service/config/base-configuration";
+import { Node } from "@kie/build-chain-configuration-reader";
+import { TreatmentOptions } from "@bc/domain/treatment-options";
 
 @Service()
 export class ConfigurationService {
 
-    private logger: LoggerService;
     private configuration: BaseConfiguration;
 
     constructor() {
-        this.logger = LoggerServiceFactory.getInstance();
         switch (Container.get(constants.CONTAINER.ENTRY_POINT)) {
             case EntryPoint.CLI:
                 this.configuration = new CLIConfiguration();
@@ -30,6 +27,9 @@ export class ConfigurationService {
         }
     }
 
+    /**
+     * Load all necessary data for the configuration object
+     */
     async init() {
         await this.configuration.init();
     }
@@ -44,12 +44,21 @@ export class ConfigurationService {
         logAndThrow("Start project needs to be defined or build chain must be run in a Github environment");
     }
 
+    /**
+     * Check whether the given node is the starter project
+     * @param node 
+     * @returns {Boolean} true if the node is the starter project
+     */
     isNodeStarter(node: Node): boolean {
         return node.project === this.getStarterProjectName();
     }
 
-    getStarterNode(chain: Node[]): Node {
-        const starterNode = chain.find(node => this.isNodeStarter(node));
+    /**
+     * Finds the starter node
+     * @returns {Node} starter node
+     */
+    getStarterNode(): Node {
+        const starterNode = this.configuration.projectList.find(node => this.isNodeStarter(node));
         if (!starterNode) {
             const errorMessage = `There's no project ${this.getStarterProjectName()} in the chain. This is normally due the project starting 
                                   the job (or the one selected to behave like so it's not in the project tree information. Please choose a 
@@ -59,17 +68,42 @@ export class ConfigurationService {
         return starterNode;
     }
 
-    getNodeExecutionLevel(node: Node, chain: Node[]): NodeExecutionLevel {
-        const starterNodeIndex = chain.indexOf(this.getStarterNode(chain));
-        const currentNodeIndex = chain.indexOf(node);
+    /**
+     * Gets the execution level (current, upstream or downstream) for the given node
+     * @param node 
+     * @returns {NodeExecutionLevel} Upstream, current or downstream
+     */
+    getNodeExecutionLevel(node: Node): NodeExecutionLevel {
+        const starterNodeIndex = this.configuration.projectList.indexOf(this.getStarterNode());
+        const currentNodeIndex = this.configuration.projectList.indexOf(node);
         return currentNodeIndex < starterNodeIndex ? NodeExecutionLevel.UPSTREAM : currentNodeIndex > starterNodeIndex ? NodeExecutionLevel.DOWNSTREAM : NodeExecutionLevel.CURRENT;
     }
 
+    /**
+     * Checks whether execution needs to be skipped for the given node
+     * @param node 
+     * @returns {Boolean} true if execution needs to be skipped otherwise false
+     */
     skipExecution(node: Node): boolean {
         return this.configuration.parsedInputs.skipExecution ?
                 this.configuration.parsedInputs.skipExecution :
                 this.configuration.parsedInputs.skipProjectExecution ?
                 this.configuration.parsedInputs.skipProjectExecution.includes(node.project) :
                 false;
+    }
+
+    /**
+     * Parses user input from custom command treatment option to create the treatment option object
+     * @returns {TreatmentOptions} Construct the treatment options domain object
+     */
+    getTreatmentOptions(): TreatmentOptions {
+        if (this.configuration.parsedInputs.customCommandTreatment) {
+            const replaceExpressions = this.configuration.parsedInputs.customCommandTreatment.split("||");
+            if (replaceExpressions.length !== 2) {logAndThrow("Invalid custom command treatment format. The format required is <Regex||ReplacementRegex>");}
+            return {
+                replaceExpressions
+            };
+        }
+        return {};
     }
 }
--- test/unitary/service/config/configuration-service.test.ts
+++ test/unitary/service/config/configuration-service.test.ts
@@ -8,6 +8,9 @@ import Container from "typedi";
 import path from "path";
 import { BaseConfiguration } from "@bc/service/config/base-configuration";
 import fs from "fs";
+import { Node } from "@kie/build-chain-configuration-reader";
+import { NodeExecutionLevel } from "@bc/domain/node-execution-level";
+import { TreatmentOptions } from "@bc/domain/treatment-options";
 
 // disable logs
 jest.spyOn(global.console, "log");
@@ -26,20 +29,25 @@ afterEach(() =>{
 describe("cli", () => {
     let config: ConfigurationService;
     let currentInput: InputValues;
+    const startProject = "owner/project";
     
     beforeAll(() => {
         Container.set(constants.CONTAINER.ENTRY_POINT, EntryPoint.CLI);
     });
     
     beforeEach(async () => {
-        currentInput = {...defaultInputValues, startProject: "owner/project", url: "https://github.com/owner/project/pull/270"};
+        currentInput = {...defaultInputValues, startProject, url: "https://github.com/owner/project/pull/270"};
         jest.spyOn(BaseConfiguration.prototype, "parsedInputs", "get").mockImplementation(() => currentInput);
         config = new ConfigurationService();
         await config.init();
     });
 
+    afterEach(() => {
+        currentInput = {...defaultInputValues, startProject, url: "https://github.com/owner/project/pull/270"};
+    });
+
     test("getStarterProjectName: success", () => {
-        expect(config.getStarterProjectName()).toBe("owner/project");
+        expect(config.getStarterProjectName()).toBe(startProject);
     });
 
     test("getStarterProjectName: failure", () => {
@@ -47,12 +55,73 @@ describe("cli", () => {
         delete currentInput["startProject"];
         expect(() => config.getStarterProjectName()).toThrowError();
     });
+
+    test.each([
+        [true, startProject],
+        [false, "falsename"]
+    ])("isNodeStarter %p", (isNodeStarter: boolean, project: string) => {
+        expect(config.isNodeStarter({project: project})).toBe(isNodeStarter);
+    });
+
+    test("getStarterNode: success", () => {
+        const chain: Node[] = [
+            {project: "abc"},
+            {project: startProject},
+            {project: "def"}
+        ];
+        const nodeFound: Node  = {project: startProject};
+        jest.spyOn(BaseConfiguration.prototype, "projectList", "get").mockImplementation(() => chain);
+        expect(config.getStarterNode()).toStrictEqual(nodeFound);
+    });
+
+    test("getStarterNode: failure", () => {
+        const chain: Node[] = [
+            {project: "abc"},
+            {project: "xyz"},
+            {project: "def"}
+        ];
+        jest.spyOn(BaseConfiguration.prototype, "projectList", "get").mockImplementation(() => chain);
+        expect(() => config.getStarterNode()).toThrowError();
+    });
+
+    test.each([
+        ["upstream", 0, NodeExecutionLevel.UPSTREAM],
+        ["current", 1, NodeExecutionLevel.CURRENT],
+        ["downstream", 2, NodeExecutionLevel.DOWNSTREAM]
+    ])("getNodeExecutionLevel: %p", (title: string, currNodeIndex: number, executionLevel: NodeExecutionLevel) => {
+        const chain: Node[] = [{project: "abc"}, {project: startProject}, {project: "def"}];
+        jest.spyOn(BaseConfiguration.prototype, "projectList", "get").mockImplementation(() => chain);
+        expect(config.getNodeExecutionLevel(chain[currNodeIndex])).toBe(executionLevel);
+    });
+
+    test.each([
+        ["option skipExecution set to true", {project: "abc"}, {...defaultInputValues, skipExecution: true}, true],
+        ["option skipExecution set to false and skipProjectExecution contains project name", {project: "abc"}, {...defaultInputValues, skipProjectExecution: ["abc"]}, true],
+        ["option skipExecution set to false and skipProjectExecution does not contain project name", {project: "abc"}, {...defaultInputValues, skipProjectExecution: ["def"]}, false],
+        ["option skipExecution set to false and skipProjectExecution not defined", {project: "abc"}, defaultInputValues, false],
+    ])("skipExecution: %p", (title: string, node: Node, currInput: InputValues, isExecutionSkipped: boolean) => {
+        currentInput = currInput;
+        expect(config.skipExecution(node)).toBe(isExecutionSkipped);
+    });
+
+    test.each([
+        ["custom command treatment defined", {...defaultInputValues, customCommandTreatment: "abc||xyz"}, {replaceExpressions: ["abc", "xyz"]}],
+        ["custom command treatment not defined", defaultInputValues, {}]
+    ])("getTreatmentOptions: success - %p", (title: string, currInput: InputValues, treatmentOptions: TreatmentOptions) => {
+        currentInput = currInput;
+        expect(config.getTreatmentOptions()).toStrictEqual(treatmentOptions);
+    });
+
+    test("getTreatmentOptions: failure - custom command treatment defined but incorrent format", () => {
+        currentInput = {...defaultInputValues, customCommandTreatment: "abc"};
+        expect(() => config.getTreatmentOptions()).toThrowError();
+    });
 });
 
 describe("action", () => {
     let config: ConfigurationService;
     let currentInput: InputValues;
-    
+    const env = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), "utf8")).env;
     beforeAll(() => {
         Container.set(constants.CONTAINER.ENTRY_POINT, EntryPoint.GITHUB_EVENT);
     });
@@ -64,13 +133,77 @@ describe("action", () => {
         await config.init();
     });
 
+    afterEach(() => {
+        currentInput = defaultInputValues;
+    });
+
     test("getStarterProjectName: success", () => {
-        const actualData = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), "utf8")).env.repository;
-        expect(config.getStarterProjectName()).toBe(actualData);
+        expect(config.getStarterProjectName()).toBe(env.repository);
     });
 
     test("getStarterProjectName: failure", () => {
         delete process.env["GITHUB_REPOSITORY"];
         expect(() => config.getStarterProjectName()).toThrowError();
+    });
+
+    test.each([
+        [true, env.repository],
+        [false, "falsename"]
+    ])("isNodeStarter %p", (isNodeStarter: boolean, project: string) => {
+        expect(config.isNodeStarter({project: project})).toBe(isNodeStarter);
+    });
+
+    test("getStarterNode: success", () => {
+        const chain: Node[] = [
+            {project: "abc"},
+            {project: env.repository},
+            {project: "def"}
+        ];
+        const nodeFound: Node  = {project: env.repository};
+        jest.spyOn(BaseConfiguration.prototype, "projectList", "get").mockImplementation(() => chain);
+        expect(config.getStarterNode()).toStrictEqual(nodeFound);
+    });
+
+    test("getStarterNode: failure", () => {
+        const chain: Node[] = [
+            {project: "abc"},
+            {project: "xyz"},
+            {project: "def"}
+        ];
+        jest.spyOn(BaseConfiguration.prototype, "projectList", "get").mockImplementation(() => chain);
+        expect(() => config.getStarterNode()).toThrowError();
+    });
+
+    test.each([
+        ["upstream", 0, NodeExecutionLevel.UPSTREAM],
+        ["current", 1, NodeExecutionLevel.CURRENT],
+        ["downstream", 2, NodeExecutionLevel.DOWNSTREAM]
+    ])("getNodeExecutionLevel: %p", (title: string, currNodeIndex: number, executionLevel: NodeExecutionLevel) => {
+        const chain: Node[] = [{project: "abc"}, {project: env.repository}, {project: "def"}];
+        jest.spyOn(BaseConfiguration.prototype, "projectList", "get").mockImplementation(() => chain);
+        expect(config.getNodeExecutionLevel(chain[currNodeIndex])).toBe(executionLevel);
+    });
+
+    test.each([
+        ["option skipExecution set to true", {project: "abc"}, {...defaultInputValues, skipExecution: true}, true],
+        ["option skipExecution set to false and skipProjectExecution contains project name", {project: "abc"}, {...defaultInputValues, skipProjectExecution: ["abc"]}, true],
+        ["option skipExecution set to false and skipProjectExecution does not contain project name", {project: "abc"}, {...defaultInputValues, skipProjectExecution: ["def"]}, false],
+        ["option skipExecution set to false and skipProjectExecution not defined", {project: "abc"}, defaultInputValues, false],
+    ])("skipExecution: %p", (title: string, node: Node, currInput: InputValues, isExecutionSkipped: boolean) => {
+        currentInput = currInput;
+        expect(config.skipExecution(node)).toBe(isExecutionSkipped);
+    });
+
+    test.each([
+        ["custom command treatment defined", {...defaultInputValues, customCommandTreatment: "abc||xyz"}, {replaceExpressions: ["abc", "xyz"]}],
+        ["custom command treatment not defined", defaultInputValues, {}]
+    ])("getTreatmentOptions: success - %p", (title: string, currInput: InputValues, treatmentOptions: TreatmentOptions) => {
+        currentInput = currInput;
+        expect(config.getTreatmentOptions()).toStrictEqual(treatmentOptions);
+    });
+
+    test("getTreatmentOptions: failure - custom command treatment defined but incorrent format", () => {
+        currentInput = {...defaultInputValues, customCommandTreatment: "abc"};
+        expect(() => config.getTreatmentOptions()).toThrowError();
     });
 });

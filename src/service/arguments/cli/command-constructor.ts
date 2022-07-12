d6704a9f0c4926b8c2617978a6b405c2aefb9dd4
Shubh Bapna <sbapna@redhat.com>
2022-07-12 12:31:22 -0400

    linted arguments service

--- src/service/arguments/cli/command-constructor.ts
+++ src/service/arguments/cli/command-constructor.ts
@@ -1,9 +1,9 @@
 import { Command } from "commander";
 
 export interface CommandConstructor {
-     /**
-     * Construct the command line argument parser for the cli
-     * @returns {Command} Command line argument parser object
-     */
-    createCommand(): Command
+  /**
+   * Construct the command line argument parser for the cli
+   * @returns {Command} Command line argument parser object
+   */
+  createCommand(): Command;
 }
--- src/service/arguments/cli/main-command-factory.ts
+++ src/service/arguments/cli/main-command-factory.ts
@@ -2,47 +2,44 @@ import { BuildSubCommandFactory } from "@bc/service/arguments/cli/build/build-su
 import { ToolSubCommandFactory } from "@bc/service/arguments/cli/tools/tool-subcommand-factory";
 import { Command } from "commander";
 
-
 /**
  * Constructs and compiles all the subcommands together to produce the main cli parser
  */
 export class MainCommandFactory {
-    /**
-     * Constructs the argument parser for the main command line utility
-     * @param options [OPTIONAL] configure the parser to not throw errors or write output. Useful for testing
-     * @returns {Command} Returns command parser object
-     */
-    static getCommand(options?: { exitOverride?: boolean, suppressOutput?: boolean }): Command {
-        const program = new Command();
+  /**
+   * Constructs the argument parser for the main command line utility
+   * @param options [OPTIONAL] configure the parser to not throw errors or write output. Useful for testing
+   * @returns {Command} Returns command parser object
+   */
+  static getCommand(options?: { exitOverride?: boolean; suppressOutput?: boolean }): Command {
+    const program = new Command();
 
-        program
-            .name("build-chain")
-            .description("A CLI tool to perform the build chain github actions");
-        
-        BuildSubCommandFactory.getAllCommands().forEach((cmd) => program.addCommand(this.setConfig(cmd, options)));
-        ToolSubCommandFactory.getAllCommands().forEach((cmd) => program.addCommand(this.setConfig(cmd, options)));
+    program.name("build-chain").description("A CLI tool to perform the build chain github actions");
 
-        return this.setConfig(program, options);
+    BuildSubCommandFactory.getAllCommands().forEach((cmd) => program.addCommand(this.setConfig(cmd, options)));
+    ToolSubCommandFactory.getAllCommands().forEach((cmd) => program.addCommand(this.setConfig(cmd, options)));
+
+    return this.setConfig(program, options);
+  }
+
+  /**
+   * Configure the command line parser instance according to the options passed
+   * Updates the instance to not to throw any error and suppress output
+   * Useful for testing
+   * @param program command line parser
+   * @param options additional options to configure program instance
+   * @returns {Command} configured command line parser
+   */
+  private static setConfig(program: Command, options?: { exitOverride?: boolean; suppressOutput?: boolean }): Command {
+    if (options?.exitOverride) {
+      program.exitOverride();
     }
-
-    /**
-     * Configure the command line parser instance according to the options passed
-     * Updates the instance to not to throw any error and suppress output
-     * Useful for testing
-     * @param program command line parser
-     * @param options additional options to configure program instance
-     * @returns {Command} configured command line parser
-     */
-    private static setConfig(program: Command, options?: { exitOverride?: boolean, suppressOutput?: boolean }): Command {
-        if (options?.exitOverride) {
-            program.exitOverride();
-        }
-        if (options?.suppressOutput) {
-            program.configureOutput({
-              writeOut: () => undefined,
-              writeErr: () => undefined
-            });
-        }
-        return program;
+    if (options?.suppressOutput) {
+      program.configureOutput({
+        writeOut: () => undefined,
+        writeErr: () => undefined,
+      });
     }
+    return program;
+  }
 }

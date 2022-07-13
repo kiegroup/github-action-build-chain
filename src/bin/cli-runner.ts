cb9872485bcba20ea81852309ba631ab80393fa2
Shubh Bapna <38372682+shubhbapna@users.noreply.github.com>
2022-07-13 11:06:10 -0400

    added config initialization in runner (#282)
    * added config initialization in runner
    
    * bug fixes, shifted reflect-metadata import to individual runners since those are the actual entry point
    
    * added test cases

--- src/bin/cli-runner.ts
+++ src/bin/cli-runner.ts
@@ -1,16 +1,12 @@
 #!/usr/bin/env node
 
+import "reflect-metadata";
 import { Runner } from "@bc/bin/runner";
 import { EntryPoint } from "@bc/domain/entry-point";
 
 class CLIRunner extends Runner {
-
   constructor() {
     super(EntryPoint.CLI);
-  }
-
-  initializeConfig(): void {
-    //TODO: to implement
   }
 }
 
--- src/bin/github-event-runner.ts
+++ src/bin/github-event-runner.ts
@@ -1,16 +1,12 @@
 #!/usr/bin/env node
 
+import "reflect-metadata";
 import { Runner } from "@bc/bin/runner";
 import { EntryPoint } from "@bc/domain/entry-point";
 
 class GithubEventRunner extends Runner {
-
   constructor() {
     super(EntryPoint.GITHUB_EVENT);
-  }
-
-  initializeConfig(): void {
-    //TODO: to implement
   }
 }
 

46d6d7e7e455eaad6b7f523a540b5b1764156362
Github <noreply@github.com>
2022-12-12 13:33:17 -0500

    configure octokit to use proxy if defined

--- src/service/git/octokit.ts
+++ src/service/git/octokit.ts
@@ -1,6 +1,7 @@
 import { constants } from "@bc/domain/constants";
 import { Octokit } from "@octokit/rest";
 import Container from "typedi";
+import { HttpProxyAgent } from "http-proxy-agent";
 
 /**
  * Singleton factory for octokit instance
@@ -14,9 +15,11 @@ export class OctokitFactory {
    */
   public static getOctokitInstance(): Octokit {
     if (!this.octokit) {
+      const proxy = process.env["http_proxy"];
       this.octokit = new Octokit({
         auth: Container.get(constants.GITHUB.TOKEN),
         userAgent: "kiegroup/github-build-chain-action",
+        request: { agent: proxy ? new HttpProxyAgent(proxy) : undefined },
       });
     }
     return this.octokit;

a2f7f375aa79ed89826800c50233c30c4fb0abef
Shubh Bapna <sbapna@redhat.com>
2022-12-20 11:40:14 -0500

    updated test cases

--- test/unitary/service/git/octokit.test.ts
+++ test/unitary/service/git/octokit.test.ts
@@ -1,33 +1,19 @@
+import "reflect-metadata";
 import { constants } from "@bc/domain/constants";
-import { OctokitFactory } from "@bc/service/git/octokit";
+import { OctokitService } from "@bc/service/git/octokit";
 import { Octokit } from "@octokit/rest";
 import Container from "typedi";
 
-test("Initialize a new instance", () => {
+test("Initialize a new instance without proxy", () => {
   Container.set(constants.GITHUB.TOKEN, "fake_token");
-  const get = jest.spyOn(Container, "get");
 
-  expect(OctokitFactory.getOctokitInstance()).toBeInstanceOf(Octokit);
-  expect(get).toHaveBeenCalledTimes(1);
-
-  get.mockRestore();
-  OctokitFactory.clearOctokitInstance();
+  expect(Container.get(OctokitService).octokit).toBeInstanceOf(Octokit);
 });
 
-test("Already initialized instance", () => {
+test("Initialize a new instance with proxy", () => {
   Container.set(constants.GITHUB.TOKEN, "fake_token");
-  let get = jest.spyOn(Container, "get");
+  process.env["http_proxy"] = "http://localhost:8080";
 
-  expect(OctokitFactory.getOctokitInstance()).toBeInstanceOf(Octokit);
-  expect(get).toHaveBeenCalledTimes(1);
-  get.mockRestore();
-
-  get = jest.spyOn(Container, "get");
-  Container.set(constants.GITHUB.TOKEN, "fake_token");
-
-  expect(OctokitFactory.getOctokitInstance()).toBeInstanceOf(Octokit);
-  expect(get).toHaveBeenCalledTimes(0);
-
-  get.mockRestore();
-  OctokitFactory.clearOctokitInstance();
+  expect(Container.get(OctokitService).octokit).toBeInstanceOf(Octokit);
+  delete process.env["http_proxy"];
 });

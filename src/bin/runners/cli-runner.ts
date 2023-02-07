689e06dd76295df15e295c83c0071a3a15c0e065
Shubh Bapna <sbapna@redhat.com>
2023-02-07 08:59:39 -0500

    improve error logging

--- src/bin/runners/cli-runner.ts
+++ src/bin/runners/cli-runner.ts
@@ -10,32 +10,36 @@ export class CLIRunner extends Runner {
   }
 
   async execute(): Promise<void> {
-    // parse arguments
-    const args = Container.get(CLIArguments);
-    args.getCommand().parse();
+    try {
+      // parse arguments
+      const args = Container.get(CLIArguments);
+      args.getCommand().parse();
 
-    // initialize configuration
-    await this.initConfiguration();
+      // initialize configuration
+      await this.initConfiguration();
 
-    // execute pre section
-    const preResult = await this.executePre();
-    if (preResult.isFailure) {
-      this.printExecutionFailure(preResult.output);
-      return process.exit(1);
-    }
+      // execute pre section
+      const preResult = await this.executePre();
+      if (preResult.isFailure) {
+        this.printExecutionFailure(preResult.output);
+        return process.exit(1);
+      }
 
-    // execute flow: checkout node chain -> execute commands for each phase -> upload artifacts
-    const flowResult = await this.executeFlow();
+      // execute flow: checkout node chain -> execute commands for each phase -> upload artifacts
+      const flowResult = await this.executeFlow();
 
-    // execute post section
-    const postResult = await this.executePost(flowResult.isFailure);
+      // execute post section
+      const postResult = await this.executePost(flowResult.isFailure);
 
-    if (flowResult.isFailure || postResult.isFailure) {
-      this.printNodeExecutionFailure(flowResult.output.executionResult.before);
-      this.printNodeExecutionFailure(flowResult.output.executionResult.commands);
-      this.printNodeExecutionFailure(flowResult.output.executionResult.after);
-      this.printExecutionFailure(postResult.output);
-      return process.exit(1);
+      if (flowResult.isFailure || postResult.isFailure) {
+        this.printNodeExecutionFailure(flowResult.output.executionResult.before);
+        this.printNodeExecutionFailure(flowResult.output.executionResult.commands);
+        this.printNodeExecutionFailure(flowResult.output.executionResult.after);
+        this.printExecutionFailure(postResult.output);
+        return process.exit(1);
+      }
+    } catch (err) {
+      process.exit(1);
     }
   }
 }

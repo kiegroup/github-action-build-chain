ee0754d41b8aa7ba6c4778d1d66ff188c715ed6a
Shubh Bapna <sbapna@redhat.com>
2023-03-06 16:48:01 -0500

    updated action regression to use event from pr links

--- test/e2e-regression/github-action/action.test.ts
+++ test/e2e-regression/github-action/action.test.ts
@@ -1,5 +1,6 @@
 import { Act } from "@kie/act-js";
 import { MockGithub } from "@kie/mock-github";
+import { Octokit } from "@octokit/rest";
 import { Endpoints } from "@octokit/types";
 import { readFileSync } from "fs";
 import path from "path";
@@ -24,7 +25,7 @@ type TestCommand = {
   "java-version"?: string;
   "maven-version"?: string;
   "cache-key-prefix"?: string;
-  eventPayload: PullRequestPayload;
+  event: PullRequestPayload | string;
   env?: Record<string, string>
   shouldFail?: boolean;
   matchOutput?: string[]
@@ -71,23 +72,27 @@ describe("test custom e2e github action", () => {
 
   for (const testCase of testCases) {
     test(testCase.name, async () => {
+      let eventPayload: PullRequestPayload;
+      if (typeof testCase.event === "string") {
+        eventPayload = await getEventPayload(testCase.event);
+      } else {
+        eventPayload = testCase.event;
+      }
+
       const act = new Act()
         .setGithubStepSummary("/dev/stdout")
         .setGithubToken(process.env["GITHUB_TOKEN"] ?? "token")
-        .setEnv("GITHUB_REPOSITORY", testCase.eventPayload.base.repo.full_name)
-        .setEnv("GITHUB_BASE_REF", testCase.eventPayload.base.ref);
+        .setEnv("GITHUB_REPOSITORY", eventPayload.base.repo.full_name)
+        .setEnv("GITHUB_BASE_REF", eventPayload.base.ref)
+        .setEvent({ pull_request: eventPayload });
 
       for (const key of Object.keys(testCase.env ?? {})) {
         act.setEnv(key, testCase.env![key]);
       }
-
-      if (testCase.eventPayload) {
-        act.setEvent({pull_request: testCase.eventPayload});
-      }
       
       for (const [key, value] of Object.entries(testCase)) {
         if (value && 
-          !["name", "eventPayload", "env", "name"].includes(key) &&
+          !["name", "event", "env", "shouldFail", "matchOutput"].includes(key) &&
           typeof value === "string") {
             act.setInput(key, value);
         }
@@ -98,7 +103,7 @@ describe("test custom e2e github action", () => {
         cwd: mockGithub.repo.getPath("build-chain"),
         workflowFile: mockGithub.repo.getPath("build-chain")
       });
-      
+
       expect(result.length).toBe(12);
       expect(result[8].name).toBe("Main ./build-chain");
       expect(result[8].status).toBe(testCase.shouldFail ? 1 : 0);
@@ -110,3 +115,19 @@ describe("test custom e2e github action", () => {
     });
   }
 });
+
+async function getEventPayload(event: string) {
+  const urlRegex = /^(https?:\/\/.+\/)([^/\s]+)\/([^/\s]+)\/pull\/(\d+)$/;
+  const urlCheck = event.match(urlRegex);
+  const octokit = new Octokit();
+  if (urlCheck) {    
+    const { data } = await octokit.pulls.get({
+        owner: urlCheck[2],
+        repo: urlCheck[3],
+        pull_number: parseInt(urlCheck[4]),
+      }
+    );
+    return data;
+  }
+  throw new Error("Invalid url");
+}

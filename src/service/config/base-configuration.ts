d2fa41efe21401d1e9e9bae96a811df5d9269da8
Shubh Bapna <sbapna@redhat.com>
2023-06-09 11:05:16 -0400

    added get platform config methods to return default platform

--- src/service/config/base-configuration.ts
+++ src/service/config/base-configuration.ts
@@ -1,10 +1,12 @@
 import { ToolType } from "@bc/domain/cli";
 import { EventData, GitConfiguration, ProjectConfiguration } from "@bc/domain/configuration";
 import { defaultInputValues, FlowType, InputValues } from "@bc/domain/inputs";
+import { GitTokenService } from "@bc/service/git/git-token-service";
 import { InputService } from "@bc/service/inputs/input-service";
 import { BaseLoggerService } from "@bc/service/logger/base-logger-service";
 import { LoggerService } from "@bc/service/logger/logger-service";
 import { logAndThrow } from "@bc/utils/log";
+import { DEFAULT_GITHUB_PLATFORM, DEFAULT_GITLAB_PLATFORM, Platform, PlatformType } from "@kie/build-chain-configuration-reader";
 import Container from "typedi";
 
 export abstract class BaseConfiguration {
@@ -13,10 +15,14 @@ export abstract class BaseConfiguration {
   protected _sourceProject: ProjectConfiguration;
   protected _targetProject: ProjectConfiguration;
   protected _parsedInputs: InputValues;
+  protected _defaultPlatform: PlatformType;
   protected readonly logger: BaseLoggerService;
+  protected readonly tokenService: GitTokenService;
 
   constructor() {
     this.logger = Container.get(LoggerService).logger;
+    this.tokenService = Container.get(GitTokenService);
+    this._defaultPlatform = PlatformType.GITHUB;
     this._parsedInputs = defaultInputValues;
     this._gitEventData = {};
     this._gitConfiguration = {};
@@ -72,7 +78,7 @@ export abstract class BaseConfiguration {
         branch: this.gitEventData.head.ref,
         repository: this.gitEventData.head.repo?.full_name,
         name: this.gitEventData.head.repo?.name,
-        group: this.gitEventData.head.repo?.owner.login,
+        group: this.gitEventData.head.repo?.owner?.login,
       },
       target: {
         branch: this.gitEventData.base.ref,
@@ -110,5 +116,32 @@ export abstract class BaseConfiguration {
 
   getToolType(): ToolType {
     logAndThrow("tools are defined only in CLI");
+  }
+
+  getDefaultGithubConfig(): Platform {
+    return {
+      id: this.parsedInputs?.defaultGithubId ?? DEFAULT_GITHUB_PLATFORM.id,
+      tokenId: this.parsedInputs?.defaultGithubTokenId ?? DEFAULT_GITHUB_PLATFORM.tokenId,
+      apiUrl: this.parsedInputs?.defaultGithubApiUrl ?? process.env.GITHUB_API_URL ?? DEFAULT_GITHUB_PLATFORM.apiUrl,
+      serverUrl: this.parsedInputs?.defaultGithubServeUrl ?? process.env.GITHUB_SERVER_URL ?? DEFAULT_GITHUB_PLATFORM.serverUrl,
+      type: PlatformType.GITHUB
+    };
+  }
+
+  getDefaultGitlabConfig(): Platform {
+    return {
+      id: this.parsedInputs?.defaultGitlabId ?? DEFAULT_GITLAB_PLATFORM.id,
+      tokenId: this.parsedInputs?.defaultGitlabTokenId ?? DEFAULT_GITLAB_PLATFORM.tokenId,
+      apiUrl: this.parsedInputs?.defaultGitlabApiUrl ?? process.env.CI_API_URL ?? DEFAULT_GITLAB_PLATFORM.apiUrl,
+      serverUrl: this.parsedInputs?.defaultGitlabServeUrl ?? process.env.CI_SERVER_URL ?? DEFAULT_GITLAB_PLATFORM.serverUrl,
+      type: PlatformType.GITLAB
+    };
+  }
+
+  getDefaultPlatformConfig(): Platform {
+    if (this._defaultPlatform === PlatformType.GITLAB) {
+      return this.getDefaultGitlabConfig();
+    }
+    return this.getDefaultGithubConfig();
   }
 }

7ed1476eb2ff8e38571616d8678e4e9001f3dd18
Shubh Bapna <sbapna@redhat.com>
2023-07-12 17:31:20 -0400

    implement resume

--- src/service/tools/tools-service.ts
+++ src/service/tools/tools-service.ts
@@ -2,6 +2,7 @@ import { ToolType } from "@bc/domain/cli";
 import { Tools } from "@bc/service/tools/abstract-tools";
 import { Plan } from "@bc/service/tools/plan";
 import { ProjectList } from "@bc/service/tools/project-list";
+import { Resume } from "@bc/service/tools/resume";
 import { logAndThrow } from "@bc/utils/log";
 import { Service } from "typedi";
 
@@ -15,6 +16,9 @@ export class ToolService extends Tools {
         break;
       case ToolType.PLAN:
         tool = new Plan();
+        break;
+      case ToolType.RESUME:
+        tool = new Resume();
         break;
       default:
         logAndThrow("Tool not found");
